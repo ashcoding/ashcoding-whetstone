9ac74b019a69ea0dea7effc2ab2c27dd6b2ba2f2
Ashraf <ashcoding@outlook.com>
2016-10-03 20:49:23 +0800

    New Java project

---
+++ JavaProjects/src/com/ashcoding/whetstone/Main.java
@@ -0,0 +1,9 @@
+package com.ashcoding.whetstone;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world");
+    }
+}
